ac13c54b1093571929853f6a1c72e89620f19ca9
thanh <nguyenphucthanh@live.com>
2026-08-19 10:17:24 +0700

    fix

--- src/lib/export/pdfmake-instance.ts
+++ src/lib/export/pdfmake-instance.ts
@@ -1,7 +1,11 @@
 import pdfMake from 'pdfmake/build/pdfmake'
 import pdfFonts from 'pdfmake/build/vfs_fonts'
 
-pdfMake.addVirtualFileSystem(pdfFonts)
+if (typeof pdfMake.addVirtualFileSystem === 'function') {
+  pdfMake.addVirtualFileSystem(pdfFonts)
+} else {
+  ;(pdfMake as any).vfs = pdfFonts
+}
 pdfMake.fonts = {
   Roboto: {
     normal: 'Roboto-Regular.ttf',
